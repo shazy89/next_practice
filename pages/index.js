b92628bbb470bdfb5258034390b7dd4908f44d21
shazy89 <erdo.shazy123@gmail.com>
2021-07-21 09:28:47 +0200

    link to the portfolio page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
 import React from "react";
-
+import Link from "next/link";
 const HomePage = () => {
   return (
     <div>
       <h1>Hello World</h1>
+      <ul>
+        <li>
+          <Link href="/portfolio">Portfolio</Link>
+        </li>
+      </ul>
     </div>
   );
 };
--- pages/portfolio/[projectid].js
+++ pages/portfolio/[projectid].js
@@ -1,6 +1,9 @@
 import React from "react";
-
+import { useRouter } from "next/router";
 const PortfolioIndexPage = () => {
+  const router = useRouter();
+  console.log(router.pathname);
+  console.log(router.query);
   return (
     <div>
       <h1>The Portfolio Page</h1>
