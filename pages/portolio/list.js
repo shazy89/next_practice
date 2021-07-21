ebe6f4b554dceb8401399b9c3e8dc8c1e55a362d
shazy89 <erdo.shazy123@gmail.com>
2021-07-21 09:07:08 +0200

    dynamic page

---
+++ pages/portolio/list.js
@@ -0,0 +1,12 @@
+import React from "react";
+// portfolio/list
+
+const ListPage = () => {
+  return (
+    <div>
+      <h1>This is the ListPage page</h1>
+    </div>
+  );
+};
+
+export default ListPage;
