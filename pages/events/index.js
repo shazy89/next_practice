eaea79568afe863e20d38cd506cf5f65ef2b3392
shazy89 <erdo.shazy123@gmail.com>
2021-07-22 09:03:05 +0200

    add a filter optuin to all events

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { getAllEvents } from "../../dummy-data";
 import EventList from "../../components/events/EventList";
+import EventsSearch from "../../components/events/EventsSearch";
 const AllEventsPage = () => {
   const events = getAllEvents();
   return (
-    <div>
+    <>
+      <EventsSearch />
       <EventList items={events} />
-    </div>
+    </>
   );
 };
 
