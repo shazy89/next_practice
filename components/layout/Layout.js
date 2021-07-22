cd05c1eeaa7898356a90cd4bd90842a4aa10562b
shazy89 <erdo.shazy123@gmail.com>
2021-07-22 08:41:19 +0200

    added a nav bar

---
+++ components/layout/Layout.js
@@ -0,0 +1,11 @@
+import MainHeader from "./MainHeader";
+const Layout = ({ children }) => {
+  return (
+    <>
+      <MainHeader />
+      <main>{children}</main>
+    </>
+  );
+};
+
+export default Layout;
