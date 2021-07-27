995993ecc9feb3962e2ae78118961e611a6b18ac
shazy89 <erdo.shazy123@gmail.com>
2021-07-27 17:25:21 +0200

    fetching data from the back end

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,17 @@
+import { useEffect, useState } from "react";
+import useSWR from "swr";
+
 import { getFeaturedEvents } from "../dummy-data";
 import EventList from "../components/events/EventList";
-const HomePage = () => {
+const HomePage = (props) => {
+  const [events, setSales] = useState(props.events);
+
+  const { data, error } = useSWR(
+    "https://practice-next-250c4-default-rtdb.firebaseio.com/events.json"
+  );
+  console.log(data);
   const featuredEvents = getFeaturedEvents();
-  console.log(featuredEvents);
+
   return (
     <div>
       <EventList items={featuredEvents} />
