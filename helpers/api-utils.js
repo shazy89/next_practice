621c4edb9e385eaf1e365c153d444d8600a430e8
shazy89 <erdo.shazy123@gmail.com>
2021-08-01 08:21:50 +0200

    change the functions to the async functions

--- helpers/api-utils.js
+++ helpers/api-utils.js
@@ -1,28 +1,37 @@
+export async function getAllEvents() {
+  const response = await fetch(
+    "https://nextjs-course-c81cc-default-rtdb.firebaseio.com/events.json"
+  );
+  const data = await response.json();
+
+  const events = [];
+
+  for (const key in data) {
+    events.push({
+      id: key,
+      ...data[key]
+    });
+  }
+
+  return events;
+}
+
 export async function getFeaturedEvents() {
   const allEvents = await getAllEvents();
   return allEvents.filter((event) => event.isFeatured);
 }
 
-export async function getAllEvents() {
-  const response = await fetch(
-    "https://practice-next-250c4-default-rtdb.firebaseio.com/events.json"
-  );
-  const data = await response.json();
-
-  const events = [];
-  for (const key in data) {
-    events.push({
-      id: key,
-      ...data[key] // we can use the spred operator for dynamic display of the data
-    });
-  }
-  return events;
+export async function getEventById(id) {
+  const allEvents = await getAllEvents();
+  return allEvents.find((event) => event.id === id);
 }
 
-export function getFilteredEvents(dateFilter) {
+export async function getFilteredEvents(dateFilter) {
   const { year, month } = dateFilter;
 
-  let filteredEvents = DUMMY_EVENTS.filter((event) => {
+  const allEvents = await getAllEvents();
+
+  let filteredEvents = allEvents.filter((event) => {
     const eventDate = new Date(event.date);
     return (
       eventDate.getFullYear() === year && eventDate.getMonth() === month - 1
@@ -30,9 +39,4 @@ export function getFilteredEvents(dateFilter) {
   });
 
   return filteredEvents;
-}
-
-export async function getEventById(id) {
-  const allEvents = await getAllEvents();
-  return allEvents.find((event) => event.id === id);
 }
