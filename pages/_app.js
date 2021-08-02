443f0d4d4ef9796299dc8747c3c3c0b2b3f58753
shazy89 <erdo.shazy123@gmail.com>
2021-08-02 18:02:16 +0200

    dynamicly add the heads

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,17 @@
+import Head from "next/head";
 import "../styles/globals.css";
 import Layout from "../components/layout/Layout";
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Layout>
+        <Head>
+          <title>NextJS Events</title>
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+        </Head>
         <Component {...pageProps} />
       </Layout>
     </>
