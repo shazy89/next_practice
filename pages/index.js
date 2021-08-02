f868b249dcd166c999b27fbaf11aaa997553b50e
shazy89 <erdo.shazy123@gmail.com>
2021-08-02 12:03:55 +0200

    adding meta tags to the hed section

--- pages/index.js
+++ pages/index.js
@@ -40,7 +40,8 @@ const HomePage = (props) => {
   return (
     <div>
       <Head>
-        <title>NextJS EVENTS</title>
+        <title>NextJS Events</title>
+        <meta name="description" content="Find a lot of events here" />
       </Head>
       <EventList items={props.events} />
     </div>
